adc7b090f53b70faccb35cf5d4547c05411ced4a
Briant MC <briantmc@hotmail.com>
2024-05-25 11:58:03 -0500

    Update

--- gestionConfiguracion.tf
+++ gestionConfiguracion.tf
@@ -17,6 +17,7 @@ resource "google_compute_network" "vpc_network" {
   mtu                     = 1460
 }
 
+
 resource "google_compute_subnetwork" "default" {
   name          = "my-custom-subnet"
   ip_cidr_range = "10.0.1.0/24"
